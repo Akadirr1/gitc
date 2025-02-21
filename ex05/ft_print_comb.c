f0dbea6debab8819a5b4ca7e1398b4e5b2d7d6d9
Abdulkadir Ivenc <akadirr41@outlook.com>
2025-02-21 14:43:31 +0300

    add ex05

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+void ft_print_comb(void)
+{
+    for (int a = '0'; a <= '7'; a++)
+    {
+        for (int b = a + 1; b <= '8'; b++)
+        {
+            for (int c = b + 1; c <= '9'; c++)
+            {
+                write(1, &a, 1);
+                write(1, &b, 1);
+                write(1, &c, 1);
+                if (a != '7')
+                {
+                    write(1, ",", 1);
+                }
+            }
+        }
+    }
+}
